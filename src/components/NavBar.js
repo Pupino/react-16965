af61101407a8f5457519ad0d55a3c567c36c6ced
Pupino <romina.jalon@gmail.com>
2021-10-11 13:32:59 -0300

    adding navbar and bootstrap

---
+++ src/components/NavBar.js
@@ -0,0 +1,78 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.js';
+
+function NavBar() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">
+          DZ kinesiologia
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavDropdown"
+          aria-controls="navbarNavDropdown"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a className="nav-link active" aria-current="page" href="#">
+                Home
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Charlas
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Capacitaciones
+              </a>
+            </li>
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle"
+                href="#"
+                id="navbarDropdownMenuLink"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Consultorio Virtual
+              </a>
+              <ul
+                className="dropdown-menu"
+                aria-labelledby="navbarDropdownMenuLink"
+              >
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Consulta Evaluatoria
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Programar Turno
+                  </a>
+                </li>
+                <li>
+                  <a className="dropdown-item" href="#">
+                    Seguimiento
+                  </a>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default NavBar;
